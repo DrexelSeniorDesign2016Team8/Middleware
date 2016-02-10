f27830819315d679b4ff11227b4bc63dd91a9ff3
Jesse Kuehn <jnk1136@gmail.com>
2016-02-09 19:12:40 -0500

    password hashing test

--- create.php
+++ create.php
@@ -4,4 +4,14 @@ require_once 'start.php';
 
 // create a user account
 
+// escape email, but don't escape password--we need to hash it.
+$email = db_string($_GET['email']);
+$pass = $_GET['pass'];
+
+$prehash = hash('sha256', $pass);
+var_dump($prehash);
+
+$hash = password_hash($prehash, PASSWORD_DEFAULT);
+var_dump($hash);
+
 ?>
