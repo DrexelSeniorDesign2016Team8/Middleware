4674b8c4b586b3609169bca3d2d3566ad390975d
unknown <U6015386@U6015386-TPL-A.ten.thomsonreuters.com>
2016-02-29 13:50:42 -0500

    test

--- addFavorite.php
+++ addFavorite.php
@@ -3,6 +3,7 @@ require_once 'setup.php';
 
 	$SID = $_GET['sid'];
 	$UID = $_GET['college_id'];
-	$userID = mysql_result($DB->query("select UserID from user_sessions where SessionID = $SID"),0,"UserID");
+	$result = $DB->query("select UserID from user_sessions where SessionID = $SID");
+	$userID = mysql_result($result,0,"UserID");
 	echo $userID . "\n";//$query = "INSERT INTO users_favorites (UserID,InstID) VALUES($userID,$SID)";
 ?>
