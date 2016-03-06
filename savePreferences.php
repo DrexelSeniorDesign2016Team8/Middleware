d58cb1be532ee3e9034353c9bb8e1434d4c50fc8
unknown <U6015386@U6015386-TPL-A.ten.thomsonreuters.com>
2016-03-05 23:48:16 -0500

    Fixing syntax error

--- savePreferences.php
+++ savePreferences.php
@@ -28,7 +28,7 @@ require_once 'session.class.php';
     if(empty($zip)) {
 		$zip = "null";
 	}
-    $DB->query("delete from users_info where UserID = $user"))
+    $DB->query("delete from users_info where UserID = $user");
     if($DB->query("insert into users_info (UserID,GPA,SATMath,SATReading,ACT,ZIP,State) VALUES($user,$GPA,$math,$reading,$ACT,'$zip','$state'")) {
     	print "true";
     } else {
