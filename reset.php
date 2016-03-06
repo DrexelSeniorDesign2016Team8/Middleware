e0fc0b33f2d016224256a4f7307ce50bc0b85fa6
Jesse Kuehn <jnk1136@gmail.com>
2016-03-06 14:08:33 -0500

    password reset

---
+++ reset.php
@@ -0,0 +1,26 @@
+<?php
+require_once 'setup.php'
+
+$email = db_string($_GET['email']);
+
+
+$DB->query("
+	SELECT FROM users
+	WHERE email = '$email'");
+
+if ($DB->has_results()) {
+				$pass = substr(hash('sha256', rand()), 0, 12);
+				$passhash = password_hash(hash('sha256'), $pass);
+
+				$DB->query("
+					UPDATE users
+					SET Password = ' $passhash'");
+
+				$msg = "Your password on searchcollege.me has been reset.\r\n\r\nYour new password is $pass\r\n.";
+
+				mail($email, 'College Search Password Reset', $msg);
+}
+
+echo json_encode(array('status' => 'success', 'response' => 'Password reset.'));
+
+?>
