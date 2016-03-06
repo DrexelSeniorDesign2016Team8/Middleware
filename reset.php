ce8c1b4233317ac3586905f2d281dd7a75dbc066
Jesse Kuehn <jnk1136@gmail.com>
2016-03-06 14:19:18 -0500

    SELECT properly

--- reset.php
+++ reset.php
@@ -5,20 +5,22 @@ $email = db_string($_GET['email']);
 
 
 $DB->query("
-	SELECT FROM users
+	SELECT ID
+	FROM users
 	WHERE email = '$email'");
 
 if ($DB->has_results()) {
-				$pass = substr(hash('sha256', rand()), 0, 12);
-				$passhash = password_hash(hash('sha256'), $pass);
+	$pass = substr(hash('sha256', rand()), 0, 12);
+	$passhash = password_hash(hash('sha256'), $pass);
 
-				$DB->query("
-					UPDATE users
-					SET Password = ' $passhash'");
+	$DB->query("
+		UPDATE users
+		SET Password = ' $passhash'
+		WHERE email = '$email'");
 
-				$msg = "Your password on searchcollege.me has been reset.\r\n\r\nYour new password is $pass\r\n.";
+	$msg = "Your password on searchcollege.me has been reset.\r\n\r\nYour new password is $pass\r\n.";
 
-				mail($email, 'College Search Password Reset', $msg);
+	mail($email, 'College Search Password Reset', $msg);
 }
 
 echo json_encode(array('status' => 'success', 'response' => 'Password reset.'));
