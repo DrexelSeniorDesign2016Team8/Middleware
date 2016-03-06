2a3882c2196d21d884e6843fb14e83a244d1e034
Jesse Kuehn <jnk1136@gmail.com>
2016-03-06 14:24:17 -0500

    hash properly

--- reset.php
+++ reset.php
@@ -11,11 +11,11 @@ $DB->query("
 
 if ($DB->has_results()) {
 	$pass = substr(hash('sha256', rand()), 0, 12);
-	$passhash = password_hash(hash('sha256'), $pass);
+	$passhash = password_hash(hash('sha256', $pass));
 
 	$DB->query("
 		UPDATE users
-		SET Password = ' $passhash'
+		SET Password = '$passhash'
 		WHERE email = '$email'");
 
 	$msg = "Your password on searchcollege.me has been reset.\r\n\r\nYour new password is $pass\r\n.";
