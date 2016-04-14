94e2a619d5218ba395cdfa5b75524a2818fd504f
unknown <U6015386@U6015386-TPL-A.ten.thomsonreuters.com>
2016-04-14 13:17:24 -0400

    fixing up query

--- getInstDetails.php
+++ getInstDetails.php
@@ -2,11 +2,11 @@
 require_once 'setup.php';
 
 	$instID = $_GET['instID'];
-	$sid = $_GET['sid'];
+	$sid = $_GET['SID'];
 	if(empty($sid)) {
         $sid = "0";
     }
-    $query = "select institutions.ID as instID, institutions.Name as name, concat(institutions.Address, ', ',institutions.City, ' ', institutions.State, ' ', institutions.Zip) as address, institutions.Phone as phoneNumber, institutions.Population as population, institutions.URL as URL,(CASE when exists(select users_favorites.InstID from users_favorites,user_sessions where user_sessions.SessionID = " . $sid . " and user_sessions.UserID = users_favorites.UserID and users_favorites.InstID = $instID) then 1 else 0 end) as favorited from users_favorites,user_sessions,institutions where user_sessions.SessionID = $sid and users_favorites.UserID = user_sessions.UserID and users_favorites.InstID = institutions.ID";
+    $query = "select institutions.ID as instID, institutions.Name as name, concat(institutions.Address, ', ',institutions.City, ' ', institutions.State, ' ', institutions.Zip) as address, institutions.Phone as phoneNumber, institutions.Population as population, institutions.URL as URL,(CASE when exists(select users_favorites.InstID from users_favorites,user_sessions where user_sessions.SessionID = " . $sid . " and user_sessions.UserID = users_favorites.UserID and users_favorites.InstID = $instID) then 1 else 0 end) as favorited, institutions_scores.GPA as GPA, institutions_scores.SATMath as SATMath, institutions_scores.SATReading as SATReading, institutions_scores.SATWriting as SATWriting, institutions_scores.ACT as ACT from users_favorites,user_sessions,institutions,institutions_scores where user_sessions.SessionID = $sid and institutions.ID = $instID and institutions_scores.instID = institutions.ID";
     print($query);
 	$result = $DB->query($query);
 	$rows = array();
