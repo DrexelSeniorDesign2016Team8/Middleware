56b7bab0ec000f2f5a9909346112228cae484188
Jesse Kuehn <jnk1136@gmail.com>
2016-05-03 19:28:50 -0400

    Im not 100% sure this will work

--- search.php
+++ search.php
@@ -15,7 +15,13 @@ function doSearch()
         $SID = "0";
     }
     //Create beginning part of query
-    $query = "select SQL_CALC_FOUND_ROWS institutions.ID as instID, institutions.ID as instIDs, institutions.Name as name,  (CASE when exists(select users_favorites.InstID from users_favorites,user_sessions where user_sessions.SessionID = " . $SID . " and user_sessions.UserID = users_favorites.UserID and users_favorites.InstID = instIDs) then 1 else 0 end) as favorited from institutions, institutions_scores, users_favorites, user_sessions where institutions.ID = institutions_scores.InstID";
+    //$query = "select SQL_CALC_FOUND_ROWS institutions.ID as instID, institutions.ID as instIDs, institutions.Name as name,  (CASE when exists(select users_favorites.InstID from users_favorites,user_sessions where user_sessions.SessionID = " . $SID . " and user_sessions.UserID = users_favorites.UserID and users_favorites.InstID = instIDs) then 1 else 0 end) as favorited from institutions, institutions_scores, users_favorites, user_sessions where institutions.ID = institutions_scores.InstID";
+
+		$query = "SELECT SQL_CALC_FOUND_ROWS institutions.ID, institutions.name, (SELECT COUNT(users_favorites.UserID) FROM users_favorites JOIN user_sessions ON user_sessions.UserID = users_favorites.UserID WHERE user_sessions.SessionID = '$SID' AND user_favorites.InstID = institutions.ID) as favorited
+			FROM institutions
+			JOIN institutions_scores ON institutions.ID = institutions_scores.InstID
+			WHERE";
+
     //Get variables of call
     //$writing = $_GET['WritingScore'];
 
@@ -44,55 +50,60 @@ function doSearch()
     //Just prepend sql messages for now
     //Need to check that inputs are correct eventually
     //Check if a value exists for a parameter, if it does change the query
+		$query_parts = array();
     if (!empty($GPA)) {
-        $query = $query . " and institutions_scores.GPA <= " . $GPA;
+			array_push($query_parts, "institution_scores.GPA <= $GPA");
     }
     if (!empty($ACT)) {
-        $query = $query . " and institutions_scores.ACT <= " . $ACT;
+			array_push($query_parts, "institution_scores.ACT <= $ACT");
     }
     if (!empty($math)) {
-        $query = $query . " and institutions_scores.SATMath <= " . $math;
+			array_push($query_parts, "institution_scores.SATMath <= $math");
     }
     if (!empty($reading)) {
-        $query = $query . " and institutions_scores.SATReading <= " . $reading;
-    }
-    if (!empty($name)) {
-        $query = $query . " and institutions.Name like \"%" . $name . "%\"";
-    }
-    if (!empty($state)) {
-        $query = $query . " and institutions.State = \"" . $state . "\"";
-    }
-    if (!empty($address)) {
-        $query = $query . " and institutions.Address like \"%" . $address . "%\"";
-    }
-    if (!empty($accept)) {
-        $query = $query . " and institutions.Acceptance >= " . $accept;
-    }
-    if (!empty($zip)) {
-        $query = $query . " and institutions.ZIP = \"" . $zip . "\"";
-    }
-    if (!empty($retention)) {
-        $query = $query . " and institutions.Retention >= " . $retention;
-    }
-    if (!empty($type)) {
-        $query = $query . " and institutions.Type = \"" . $type . "\"";
-    }
-    if (!empty($minPop) && !empty($maxPop)) {
-        $query = $query . " and institutions.Population BETWEEN " . $minPop . " and " . $maxPop;
-    }
-    if (!empty($minClass) && !empty($maxClass)) {
-        $query = $query . " and institutions.ClassSize BETWEEN " . $minClass . " and " . $maxClass;
+			array_push($query_parts, "institution_scores.SATReading <= $reading");
     }
     if (!empty($writing)) {
-        $query = $query . " and institutions_scores.SATWriting <= " . $writing;
+			array_push($query_parts, "institution_scores.SATWriting <= $writing");
+    }
+    if (!empty($name)) {
+			array_push($query_parts, "institutions.Name like \"%" . $name . "%\"");
+    }
+    if (!empty($state)) {
+			array_push($query_parts, "institutions.state = '$state'");
+    }
+    if (!empty($address)) {
+			array_push($query_parts, "institutions.Address like \"%" . $address . "%\"");
+    }
+    if (!empty($accept)) {
+			array_push($query_parts, "institutions.Acceptance >= $accept");
+    }
+    if (!empty($zip)) {
+			array_push($query_parts, "institutions.ZIP = '$zip'");
+    }
+    if (!empty($retention)) {
+			array_push($query_parts, "institutions.Retention >= $retention");
+    }
+    if (!empty($type)) {
+			array_push($query_parts, "institutions.Type = '$type'");
+    }
+    if (!empty($minPop) && !empty($maxPop)) {
+			array_push($query_parts, "institutions.Population BETWEEN $minpop AND $maxpop");
+    }
+    if (!empty($minClass) && !empty($maxClass)) {
+			array_push($query_parts, "institutions.ClassSize BETWEEN $minClass AND $maxClass");
     }
     if (!empty($commonApp)) {
-        $query = $query . " and institutions.CommonApp = 1";
+			array_push($query_parts, "institutions.CommonApp = 1");
     }
     if (!empty($favorites)) {
-        $query = $query . " and user_sessions.SessionID = $SID and users_favorites.UserID = user_sessions.UserID and users_favorites.InstID = institutions.ID";
+        //$query = $query . " and user_sessions.SessionID = $SID and users_favorites.UserID = user_sessions.UserID and users_favorites.InstID = institutions.ID";
+				array_push("favorited = 1");
     }
-    $query = $query . " group by instID order by institutions.Name";
+
+		$query = $query . implode(" AND ", $query_parts);
+
+    $query = $query . " group by institutions.ID order by institutions.Name";
     if (!empty($page) && !empty($pageSize)) {
         $pageval = 1 + (((int)$page - 1) * (int)$pageSize);
         $query = $query . " limit $pageval,$pageSize";
