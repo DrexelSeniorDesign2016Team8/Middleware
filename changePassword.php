02a585b66786d6905d8afbff955a95c5c235696c
unknown <U6015386@U6015386-TPL-A.ten.thomsonreuters.com>
2016-05-03 19:33:56 -0400

    Adding old password param to changePassword

--- changePassword.php
+++ changePassword.php
@@ -8,7 +8,7 @@ require_once 'session.class.php';
 // escape email, but don't escape password--we need to hash it.
 $email = db_string($_GET['email']);
 $pass = $_GET['pass'];
-
+$oldpass = $_GET['oldpass'];
 $json = array();
 
 if ($email == "") {
@@ -17,8 +17,22 @@ if ($email == "") {
 } elseif ($pass === null) {
 	$json['status'] = 'error';
 	$json['error'] = 'Password is null.';
+} elseif ($oldpass == null) {
+	$json['status'] = 'error';
+	$json['error'] = 'Old password is null.';
 } else {
+	$DB->query("
+		SELECT ID, Password
+		FROM users
+		WHERE Email = '$email'");
 
+	if ($DB->has_results()) {
+		list($user_id, $old_pass_hash) = $DB->next_record();
+		$valid = password_verify(hash('sha256', $oldpass), $old_pass_hash);
+	if(!$valid) {	
+		$json['status'] = 'error';
+		$json['error'] = 'Old password is incorrect';
+	}
 	$prehash = hash('sha256', $pass);
 	$passhash = password_hash($prehash, PASSWORD_DEFAULT);
 	$DB->query("
